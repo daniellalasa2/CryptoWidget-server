14f78bcb58894e8e01511854bfa032e01e58aca2
daniel <daniel@daniels-MacBook-Air.local>
2022-03-23 22:36:53 +0300

    Crypto rates apicall implemented, rates insetion into db

--- src/Controllers/rates.controllers.js
+++ src/Controllers/rates.controllers.js
@@ -1,8 +1,9 @@
-const bcrypt = require("bcrypt");
+// const bcrypt = require("bcrypt");
 require("../node_modules/dotenv").config();
-const jwt = require("jsonwebtoken");
+// const jwt = require("jsonwebtoken");
 const RatesModel = require("../Models/rates");
 const axios = require("axios");
+MongoClient = require('mongodb').MongoClient;
 
 const getRates = axios({
 	url: process.env.RATES_API_BASEURL,
@@ -15,85 +16,89 @@ const getRates = axios({
 	headers:{
 		"Content-Type":"application/json"
 	}
-}).then(res=>{
+}).then(freshRatesObj=>{
 	console.log(res);
+	MongoClient.connect(process.env.DB_URI, function(err, db) {
+		if (err) throw err;
+		var dbo = db.db("cwdb");
+		dbo.collection("rates").insertOne(freshRatesObj, function(err, res) {
+		  if (err) throw err;
+		  console.log("1 document inserted");
+		  db.close();
+		});
+	  });
 });
  
 
 
 
-const adminLogin = (req, res, next) => {
-	Admin.find({ email: req.body.email })
-		.exec()
-		.then((admin) => {
-      console.log(admin)
-			if (admin.length < 1) {
-				return res.status(401).json({
-					message: "Auth failed: Email not found probably",
-				});
-			}
-			bcrypt.compare(req.body.password, admin[0].password, (err, result) => {
-				if (err) {
-          console.log(err)
-					return res.status(401).json({
-						message: "Auth failed",
-					});
-				}
-				if (result) {
-					const token = jwt.sign(
-						{
-              adminId: admin[0]._id,
-							email: admin[0].email,
-							name: admin[0].name,
-							phone_number: admin[0].phone_number,
-						},
-						process.env.jwtSecret,
-						{
-							expiresIn: "1d",
-						}
-          );
-          console.log(admin[0])
-					return res.status(200).json({
-						message: "Auth successful",
-						adminDetails: {
-							adminId: admin[0]._id,
-							name: admin[0].name,
-							email: admin[0].email,
-							phone_number: admin[0].phone_number,
-						},
-						token: token,
-					});
-				}
-				res.status(401).json({
-					message: "Auth failed1",
-				});
-			});
-		})
-		.catch((err) => {
-			res.status(500).json({
-				error: err,
-			});
-		});
-}
+// const adminLogin = (req, res, next) => {
+// 	Admin.find({ email: req.body.email })
+// 		.exec()
+// 		.then((admin) => {
+//       console.log(admin)
+// 			if (admin.length < 1) {
+// 				return res.status(401).json({
+// 					message: "Auth failed: Email not found probably",
+// 				});
+// 			}
+// 			bcrypt.compare(req.body.password, admin[0].password, (err, result) => {
+// 				if (err) {
+//           console.log(err)
+// 					return res.status(401).json({
+// 						message: "Auth failed",
+// 					});
+// 				}
+// 				if (result) {
+// 					const token = jwt.sign(
+// 						{
+//               adminId: admin[0]._id,
+// 							email: admin[0].email,
+// 							name: admin[0].name,
+// 							phone_number: admin[0].phone_number,
+// 						},
+// 						process.env.jwtSecret,
+// 						{
+// 							expiresIn: "1d",
+// 						}
+//           );
+//           console.log(admin[0])
+// 					return res.status(200).json({
+// 						message: "Auth successful",
+// 						adminDetails: {
+// 							adminId: admin[0]._id,
+// 							name: admin[0].name,
+// 							email: admin[0].email,
+// 							phone_number: admin[0].phone_number,
+// 						},
+// 						token: token,
+// 					});
+// 				}
+// 				res.status(401).json({
+// 					message: "Auth failed1",
+// 				});
+// 			});
+// 		})
+// 		.catch((err) => {
+// 			res.status(500).json({
+// 				error: err,
+// 			});
+// 		});
+// }
 
-const getMe = async (req, res) => {
-	const adminId = req.admin.adminId;
-	const admin = await Admin.findById(adminId);
-	if (admin) {
-		res.status(200).json({
-			message: "Found",
-			admin,
-		});
-	} else {
-		res.status(400).json({
-			message: "Bad request",
-		});
-	}
-};
+// const getMe = async (req, res) => {
+// 	const adminId = req.admin.adminId;
+// 	const admin = await Admin.findById(adminId);
+// 	if (admin) {
+// 		res.status(200).json({
+// 			message: "Found",
+// 			admin,
+// 		});
+// 	} else {
+// 		res.status(400).json({
+// 			message: "Bad request",
+// 		});
+// 	}
+// };
 
-module.exports = {
-  adminLogin,
-  adminRegister,
-	getMe,
-	getRates
-};
+module.exports = {getRates};
