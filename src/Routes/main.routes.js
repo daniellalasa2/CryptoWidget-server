f5601af353f6e5a77869e2ba3064af70bd595ec5
daniel <daniel@daniels-MacBook-Air.local>
2022-03-23 22:37:24 +0300

    Rates route added

---
+++ src/Routes/main.routes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+// const checkAuth = require('../Middleware/checkAuth.middleware');
+const ratesControllers = require('../Controllers/rates.controllers');
+const router = express.Router();
+
+router.get('/coinrates', ratesControllers.getRates);
+// router.post('/login', adminControllers.adminLogin);
+// router.get('/me', checkAuth, adminControllers.getMe);
+
+module.exports = router
